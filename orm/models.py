72305160026a4ee96fa914fce891de83a59ececc
unknown <bilalpt>
2023-07-26 21:17:20 +0530

    all objects and sql query check

--- orm/models.py
+++ orm/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Teacher(models.Model):
+    firstname = models.CharField(max_length=255)
+    surname   = models.CharField(max_length=255)
+
+    def __init__(self):
+        return self.firstname
+    
+class Student(models.Model):
+    firstname = models.CharField(max_length=255)
+    surname   = models.CharField(max_length=255)
+    age       = models.IntegerField()
+    classroom = models.IntegerField()
+    teacher   = models.CharField(max_length=100)
+
+
+    def __init__(self):
+        return self.firstname
+        
--- orm/views.py
+++ orm/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Student
+from django.db import connection
 
 # Create your views here.
+
+def student(request):
+    students=Student.objects.all()
+    print(students)
+    print(students.query)
+    print(connection.queries)
+
+    return render(request,'student.html',{'students':students})
